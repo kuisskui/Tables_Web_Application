818cdf57b6fe50f58afe31e1e797e62543900e5f
kuikaopong <kuikaopong.2002@gmail.com>
2023-01-03 13:11:55 +0700

    create lobby page

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path("", views.lobby, name="lobby"),
     path("admin/", admin.site.urls),
-    path("Tables/", include("Tables.urls"))
+    path("Tables/", include("Tables.urls")),
 ]
