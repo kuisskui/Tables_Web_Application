211746e413c7df7883aacd83338aad39aca55c0a
kuikaopong <kuikaopong.2002@gmail.com>
2023-01-04 15:50:19 +0700

    create exercises and contents

--- English/views.py
+++ English/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'English/home.html')
+
+def exercise(request, unit):
+    return render(request, f'English/basic_english/exercises/exercise{unit}.html')
